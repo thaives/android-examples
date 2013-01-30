b506d9f510eb5d5308ad1096fec5c53c6772b5d6
tacksoo <tacksoo.im@gmail.com>
2013-01-30 09:42:51 -0500

    Updated MainActivity

--- src/com/example/helloworld/MainActivity.java
+++ src/com/example/helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -13,6 +14,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		catButton = (Button) findViewById(R.id.catButton);
+		catButton.setOnClickListener(new MyOnClickListener());
 	}
 
 	@Override
@@ -20,6 +23,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public class MyOnClickListener implements View.OnClickListener {
+		@Override
+		public void onClick(View arg0) {
+			
+		}
 	}
 
 }
