1ea7a3efafbc034a5b45c42beeca6ace03244145
crystalist <crystalist@gmail.com>
2013-01-30 01:54:05 -0500

    initial commit

---
+++ src/com/example/helloworld/CheckBoxActivity.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CheckBoxActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
